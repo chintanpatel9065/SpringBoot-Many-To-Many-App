c076b33e135ae7fa6e5741cb3ee634a436c56dbe
chintanpatel <chintanpatel9065@gmail.com>
2026-01-05 16:05:48 +0530

    Initialize Spring Boot Many-to-Many application with Maven configuration, PostgreSQL setup, and project structure.

---
+++ src/main/java/org/chintanpatel/springbootmanytomanyapp/SpringBootManyToManyAppApplication.java
@@ -0,0 +1,13 @@
+package org.chintanpatel.springbootmanytomanyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManyToManyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootManyToManyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chintanpatel/springbootmanytomanyapp/SpringBootManyToManyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chintanpatel.springbootmanytomanyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootManyToManyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
