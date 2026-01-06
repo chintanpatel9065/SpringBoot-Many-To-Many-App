23337574db848d8feb9a0f501fc5ce9c8c53cb02
chintanpatel <chintanpatel9065@gmail.com>
2026-01-06 06:32:31 +0530

    Add `CourseService` interface with course management methods

---
+++ src/main/java/org/chintanpatel/springbootmanytomanyapp/course/CourseService.java
@@ -0,0 +1,19 @@
+package org.chintanpatel.springbootmanytomanyapp.course;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CourseService {
+
+    Course addCourse(Course course);
+
+    List<Course>getAllCourseList();
+
+    Optional<Course> getCourseById(Long courseId);
+
+    void deleteCourseById(Long courseId);
+
+    boolean isCourseCodeExist(String courseCode);
+
+    List<Course> searchCourses(String keyword);
+}
