d20e1af40a63a4a136e1b257b4c66aa676512b78
chintanpatel <chintanpatel9065@gmail.com>
2026-01-06 06:36:09 +0530

    Add `Enrollment` entity with fields, relationship mappings, and default values

---
+++ src/main/java/org/chintanpatel/springbootmanytomanyapp/enrollment/Enrollment.java
@@ -0,0 +1,47 @@
+package org.chintanpatel.springbootmanytomanyapp.enrollment;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.chintanpatel.springbootmanytomanyapp.course.Course;
+import org.chintanpatel.springbootmanytomanyapp.student.Student;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "student_course")
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_course_id", nullable = false)
+    private Long enrollmentId;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Column(name = "enrolled_date", nullable = false)
+    private LocalDateTime enrolledDate;
+
+    @Column(name = "unenrolled_date")
+    private LocalDateTime unenrolledDate;
+
+    @Column(name = "status", nullable = false)
+    private String status = "ACTIVE";
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+        this.enrolledDate = LocalDateTime.now();
+        this.status = "ACTIVE";
+    }
+}
